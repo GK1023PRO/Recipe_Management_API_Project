0dfd18f50392be83662ae8cd50fc463d4e5aa3af
GK1023PRO <202311962@ua.edu.lb>
2025-05-18 22:44:53 +0300

    ma7ayt Swagger

--- src/main/java/com/recipemanagement/Main.java
+++ src/main/java/com/recipemanagement/Main.java
@@ -3,9 +3,7 @@ package com.recipemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main application entry point.
- */
+
 @SpringBootApplication
 public class Main {
     /**
