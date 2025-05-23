83ab67b9a9446d72ecc8b95eb1d48b1a20b27ba6
GK1023PRO <202311962@ua.edu.lb>
2025-05-24 02:45:41 +0300

    Last commit

--- src/main/java/com/recipemanagement/Main.java
+++ src/main/java/com/recipemanagement/Main.java
@@ -3,7 +3,23 @@ package com.recipemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Main entry point for the Recipe Management application.
+ * <p>
+ * This class bootstraps the Spring Boot application using
+ * {@link SpringApplication}. It delegates execution to the
+ * {@link RecipeManagementApplication} class.
+ * </p>
+ *
+ * <p><strong>Usage:</strong></p>
+ * <pre>{@code
+ * java -jar recipe-management.jar
+ * }</pre>
+ *
+ * @author Georges Khoury
+ * @version 1.0
+ * @since 2025-05-23
+ */
 @SpringBootApplication
 public class Main {
     /**
